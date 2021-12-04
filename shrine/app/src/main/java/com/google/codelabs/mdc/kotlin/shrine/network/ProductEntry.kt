de048b7a3590b2d5c4f42640477a344775fe4202
freeburma <kophyoblue@gmail.com>
2021-12-04 13:03:20 +1300

    102 complete

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductEntry.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductEntry.kt
@@ -11,19 +11,23 @@ import java.util.*
 /**
  * A product entry in the list of products.
  */
-class ProductEntry(
-        val title: String, dynamicUrl: String, val url: String, val price: String, val description: String) {
+class ProductEntry(val title: String, dynamicUrl: String, val url: String, val price: String, val description: String )
+{
     val dynamicUrl: Uri = Uri.parse(dynamicUrl)
 
-    companion object {
+    companion object
+    {
         /**
          * Loads a raw JSON at R.raw.products and converts it into a list of ProductEntry objects
          */
-        fun initProductEntryList(resources: Resources): List<ProductEntry> {
+        fun initProductEntryList(resources: Resources): List<ProductEntry>
+        {
             val inputStream = resources.openRawResource(R.raw.products)
-            val jsonProductsString = inputStream.bufferedReader().use(BufferedReader::readText)
+            val jsonProductsString = inputStream.bufferedReader()
+                    .use(BufferedReader::readText)
             val gson = Gson()
-            val productListType = object : TypeToken<ArrayList<ProductEntry>>() {}.type
+            val productListType = object : TypeToken<ArrayList<ProductEntry>>()
+            {}.type
             return gson.fromJson<List<ProductEntry>>(jsonProductsString, productListType)
         }
     }
