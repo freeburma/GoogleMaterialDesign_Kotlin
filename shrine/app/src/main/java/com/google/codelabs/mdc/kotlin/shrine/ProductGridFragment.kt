7df1162eb197eb904d0508cc6f1fda35d2465418
freeburma <kophyoblue@gmail.com>
2021-12-07 12:51:33 +1300

    104 done

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,8 +1,11 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,11 +18,29 @@ class ProductGridFragment : Fragment()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
+
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
+        //// Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
+
         //// Set up the toolbar
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+
+        //// Adding the motion which will drop and it will change the open/close icon automatically by "AccelerateInterpolator"
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+                                                                                activity!!,
+                                                                                view.product_grid,
+                                                                                AccelerateInterpolator(),
+                                                                                ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu), // Menu Open Icon
+                                                                                ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu)    // Menu Close Icon
+                                                                             )
+                                                 )
 
         //// Set up the Recycler VIew
         view.recycler_view.setHasFixedSize(true)
